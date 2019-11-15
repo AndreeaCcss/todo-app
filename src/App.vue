cc208c0d3e2a7254c4560d77576ed0b30aadc5f0
andreeaCcss <andreeaccss@gmail.com>
2019-11-15 14:34:38 +0100

    Making http requests

--- src/App.vue
+++ src/App.vue
@@ -10,37 +10,44 @@
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
+
 export default {
   name: "app",
   components: { Header, AddTodo, Todos },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "brush dog",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "brush cat",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "brush teeth",
-          completed: true
-        }
-      ]
+      todos: []
     };
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => {
+          console.log(res.data);
+          return (this.todos = this.todos.filter(todo => todo.id !== id));
+        })
+        .catch(console.error);
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => (this.todos = [...this.todos, res.data]))
+        .catch(console.error);
     }
+  },
+  // fired when the component is rendered
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then(res => (this.todos = res.data))
+      .catch(console.error);
   }
 };
 </script>
